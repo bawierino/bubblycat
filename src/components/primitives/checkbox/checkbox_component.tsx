212f388de26931d43a573068e6455dc93b4ff081
Alex Grethen <20316112+agex-n7@users.noreply.github.com>
2020-05-30 14:40:51 +0200

    update checkbox DataSource use

--- src/components/primitives/checkbox/checkbox_component.tsx
+++ src/components/primitives/checkbox/checkbox_component.tsx
@@ -1,19 +1,22 @@
 import { Aurum, AurumElement, DataSource } from 'aurumjs';
+import { AurumDataSource } from '../../../utils/types/data_types';
 import { BubblycatComponentPropsInternal } from '../../generic/bubblycat_component';
 
 export interface CheckboxProps {
-	checked: DataSource<boolean>;
+	checked: AurumDataSource<boolean>;
 	label?: string;
 }
 export function Checkbox(props: BubblycatComponentPropsInternal<CheckboxProps>): AurumElement {
 	const { checked, label } = props;
 
+	const onClick = () => {
+		if (checked instanceof DataSource) {
+			checked.update(!checked.value);
+		}
+	};
+
 	return (
-		<div
-			onClick={() => {
-				checked.update(!checked.value);
-			}}
-		>
+		<div onClick={onClick}>
 			<input type="checkbox" checked={checked}></input>
 			{label && <label>{label}</label>}
 		</div>
--- src/utils/types/data_types.ts
+++ src/utils/types/data_types.ts
@@ -1,6 +1,7 @@
 import { DataSource, DuplexDataSource } from 'aurumjs';
 
 export type AurumData<T> = T | DataSource<T> | DuplexDataSource<T>;
+export type AurumDataSource<T> = DataSource<T> | DuplexDataSource<T>;
 
 export type DataSourcify<T extends Object> = {
 	[key in keyof T]: DataSource<T[key]>;
